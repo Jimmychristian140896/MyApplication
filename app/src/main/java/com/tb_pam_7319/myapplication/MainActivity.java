7eefeb73c908d20b8f44d5691a5917459594251b
jimmy <jimmychristian140896@gmail.com>
2015-12-22 13:46:34 +0700

    Ubah Jimmy

--- app/src/main/java/com/tb_pam_7319/myapplication/MainActivity.java
+++ app/src/main/java/com/tb_pam_7319/myapplication/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         a="toni";
+        a="Jimmy";
     }
 
     @Override
