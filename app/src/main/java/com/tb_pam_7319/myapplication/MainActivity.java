09476b04d681548762551a35305bd81e9c353b5f
toniiindrawan <toniindrawan96@gmail.com>
2015-12-22 15:05:37 +0700

    inotika

--- app/src/main/java/com/tb_pam_7319/myapplication/MainActivity.java
+++ app/src/main/java/com/tb_pam_7319/myapplication/MainActivity.java
@@ -12,10 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         a="toni";
-        a="Indrawan";
+        a="inotika";
     }
     public String pacar() {
-        return "pacar";
+        return "pacar gueeh";
     }
 
     public String j() {
