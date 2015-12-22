219404b7c8d22268e7c35854f165854727deb435
toniiindrawan <toniindrawan96@gmail.com>
2015-12-22 13:51:38 +0700

    cangeToni

--- app/src/main/java/com/tb_pam_7319/myapplication/MainActivity.java
+++ app/src/main/java/com/tb_pam_7319/myapplication/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         a="toni";
-        a="Jimmy";
+        a="Indrawan";
     }
 
     @Override
